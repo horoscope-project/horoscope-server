22ce7e822a899c3504e22502597c578c4b2377f2
Catherine Looper <loopercat@gmail.com>
2017-11-19 20:15:15 -0800

    Added functioning API call for today's horoscope

--- server.js
+++ server.js
@@ -11,52 +11,15 @@ app.use(express.static('./public'));
 app.use(cors());
 
 
-app.get('/api/v1/countries/:iso', (req, res) => {
-  const url = `http://apiv3.iucnredlist.org/api/v3/country/getspecies/${req.params.iso}?token=${process.env.REDLIST_TOKEN}`
+app.get('/api/v1/horoscope/:sign', (req, res) => {
+  const url = `http://sandipbgt.com/theastrologer/api/horoscope/${req.params.sign}/today/`
+  console.log(url);
   superagent.get(url)
     .then(
-      speciesByCountry => res.send(speciesByCountry.text),
+      dailyHoroscope => res.send(dailyHoroscope.text),
       err => res.send(err)
     )
 });
-
-app.get('/api/v1/commonName/:name', (req, res) => {
-  const url = `http://apiv3.iucnredlist.org/api/v3/species/common_names/${req.params.name}?token=${process.env.REDLIST_TOKEN}`
-  superagent.get(url)
-    .then(
-      speciesByCountry => res.send(speciesByCountry.text),
-      err => res.send(err)
-    )
-});
-
-app.get('/api/v1/narrative/:name', (req, res) => {
-  const url = `http://apiv3.iucnredlist.org/api/v3/species/narrative/${req.params.name}?token=${process.env.REDLIST_TOKEN}`
-  superagent.get(url)
-    .then(
-      speciesByCountry => res.send(speciesByCountry.text),
-      err => res.send(err)
-    )
-});
-
-app.get('/api/v1/category/:category', (req, res) => {
-  const url = `http://apiv3.iucnredlist.org/api/v3/species/category/${req.params.category}?token=${process.env.REDLIST_TOKEN}`
-  superagent.get(url)
-    .then(
-      speciesByCategory => res.send(speciesByCategory.text),
-      err => res.send(err)
-    )
-});
-
-app.get('/api/v1/images/:name', (req, res) => {
-  const url = `https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=+${req.params.name}`
-  superagent.get(url)
-    .set('Ocp-Apim-Subscription-Key', 'e23593948467494b89ba367541f66bbc')
-    .then(
-      imageBySpecies => res.send(imageBySpecies.text),
-      err => res.send(err)
-    )
-});
-
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
